d231659857d32501bef44b60a91fe25f3d56eff1
Cleveland Flowe <flowe.c@gmail.com>
2016-08-08 19:53:42 +0000

    Added a license, url and description

--- fpm.sh
+++ fpm.sh
@@ -5,7 +5,10 @@ set -eu -o pipefail
 fpm -s dir -t rpm -n nfdump \
   -v '1.6.15+cyb' \
   --iteration 2 \
+  --license 'BSD and GPLv2+' \
+  --url https://github.com/cflowe/nfdump \
   -C ${DESTDIR:-/home/cflowe/install/nfdump} \
+  --description "Nfdump is a set of tools to collect and process NetFlow data. It's fast and has a powerful filter pcap like syntax. It supports NetFlow versions v1, v5, v7, v9 and IPFIX as well as a limited set of sflow. It includes support for CISCO ASA (NSEL) and CISCO NAT (NEL) devices which export event logging records as v9 flows. Nfdump is fully IPv6 compatible"\
   -d 'bzip2 >= 1.0.6' \
   -d 'libpcap >= 1.5.3' \
   -d 'zlib >= 1.0.2' \
