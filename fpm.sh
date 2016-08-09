244e68e0e70384e4f8eb97d2d188316d67fec627
Cleveland Flowe <flowe.c@gmail.com>
2016-08-09 17:30:33 +0000

    Moved nfdump's libs into the nfdump-lib package

--- fpm.sh
+++ fpm.sh
@@ -4,7 +4,21 @@ set -eu -o pipefail
 
 fpm -s dir -t rpm -n nfdump \
   -v '1.6.15+cyb' \
-  --iteration 2 \
+  --iteration 3 \
+  --license 'BSD and GPLv2+' \
+  --url https://github.com/cflowe/nfdump \
+  -C ${DESTDIR:-/home/cflowe/install/nfdump} \
+  --description "Nfdump is a set of tools to collect and process NetFlow data. It's fast and has a powerful filter pcap like syntax. It supports NetFlow versions v1, v5, v7, v9 and IPFIX as well as a limited set of sflow. It includes support for CISCO ASA (NSEL) and CISCO NAT (NEL) devices which export event logging records as v9 flows. Nfdump is fully IPv6 compatible"\
+  -d 'nfdump-libs = 1.6.15+cyb' \
+  -d 'bzip2 >= 1.0.6' \
+  -d 'libpcap >= 1.5.3' \
+  -d 'zlib >= 1.0.2' \
+  -d 'rrdtool >= 1.4.8' \
+  usr/bin usr/share
+
+fpm -s dir -t rpm -n nfdump-libs \
+  -v '1.6.15+cyb' \
+  --iteration 3 \
   --license 'BSD and GPLv2+' \
   --url https://github.com/cflowe/nfdump \
   -C ${DESTDIR:-/home/cflowe/install/nfdump} \
@@ -13,4 +27,4 @@ fpm -s dir -t rpm -n nfdump \
   -d 'libpcap >= 1.5.3' \
   -d 'zlib >= 1.0.2' \
   -d 'rrdtool >= 1.4.8' \
-  usr
+  usr/lib
