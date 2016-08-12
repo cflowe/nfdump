5069e0810aa8d4c0e7078abe482ff188339afa36
Cleveland Flowe <flowe.c@gmail.com>
2016-08-12 16:30:06 +0000

    .

--- fpm.sh
+++ fpm.sh
@@ -9,15 +9,17 @@ if ! type -a fpm &> /dev/null; then
 fi
 
 script_dir=$(cd "$(dirname $0)" && pwd -P)
+version='1.6.15+cyb'
+release='4'
 
 fpm -s dir -t rpm -n nfdump \
-  -v '1.6.15+cyb' \
-  --iteration 4 \
+  -v "$version" \
+  --iteration "$release" \
   --license 'BSD and GPLv2+' \
   --url https://github.com/cflowe/nfdump \
   -C ${DESTDIR:-$HOME/install/nfdump} \
   --description "Nfdump is a set of tools to collect and process NetFlow data. It's fast and has a powerful filter pcap like syntax. It supports NetFlow versions v1, v5, v7, v9 and IPFIX as well as a limited set of sflow. It includes support for CISCO ASA (NSEL) and CISCO NAT (NEL) devices which export event logging records as v9 flows. Nfdump is fully IPv6 compatible"\
-  -d 'nfdump-libs = 1.6.15+cyb' \
+  -d "nfdump-libs = ${version}-${release}" \
   -d 'bzip2 >= 1.0.6' \
   -d 'libpcap >= 1.5.3' \
   -d 'zlib >= 1.0.2' \
@@ -25,8 +27,8 @@ fpm -s dir -t rpm -n nfdump \
   usr/bin usr/share
 
 fpm -s dir -t rpm -n nfdump-libs \
-  -v '1.6.15+cyb' \
-  --iteration 4 \
+  -v "$version" \
+  --iteration "$release" \
   --license 'BSD and GPLv2+' \
   --url https://github.com/cflowe/nfdump \
   -C ${DESTDIR:-$HOME/install/nfdump} \
